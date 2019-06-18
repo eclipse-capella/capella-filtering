84efc8062a7bdfd875caccb364bce5679a096fdd
Tu Ton <minhtutonthat@gmail.com>
2019-06-18 15:08:14 +0200

    [2510] [Filtering VP] Transition LC2PC does not work
    Bug: 2510
    Change-Id: I3545bd6732f567315ed33fb30e73c3bc8f0082f6
    Signed-off-by: Tu Ton <minhtutonthat@gmail.com>

--- tests/org.polarsys.capella.filtering.tests.ju/src/org/polarsys/capella/filtering/tests/ju/tests/misc/transition/FunctionalTransitionTestCase.java
+++ tests/org.polarsys.capella.filtering.tests.ju/src/org/polarsys/capella/filtering/tests/ju/tests/misc/transition/FunctionalTransitionTestCase.java
@@ -25,6 +25,8 @@ import junit.framework.Test;
 public class FunctionalTransitionTestCase extends TopDownTransitionTestCase {
   public static final String SYSTEMFUNCTION_1 = "bc18b8d1-7b7c-4a92-8ee3-85ae431c1593"; //$NON-NLS-1$
   public static final String SF1_ASSOCIATED_FILTERING_CRITERION_SET = "e29b9987-09cb-4acb-8070-0a1abaaa7e3a"; //$NON-NLS-1$
+  public static final String LOGICALCOMPONENT_1 = "ccefe212-df01-4926-9d04-dcf1acdcc4bc"; //$NON-NLS-1$
+  public static final String LC1_ASSOCIATED_FILTERING_CRITERION_SET = "0a4ba05c-bd82-4bb2-98c6-f1736fd20299"; //$NON-NLS-1$
 
   @Override
   public List<String> getRequiredTestModels() {
@@ -36,6 +38,9 @@ public class FunctionalTransitionTestCase extends TopDownTransitionTestCase {
     EObject sf1 = getObject(SYSTEMFUNCTION_1);
     performFunctionalTransition(Arrays.asList(sf1));
     mustBeTransitioned(SF1_ASSOCIATED_FILTERING_CRITERION_SET);
+    EObject lc1 = getObject(LOGICALCOMPONENT_1);
+    performLCtoPCTransition(Arrays.asList(lc1));
+    mustBeTransitioned(LC1_ASSOCIATED_FILTERING_CRITERION_SET);
   }
 
   public static Test suite() {
