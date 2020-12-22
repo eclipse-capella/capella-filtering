472a7d20382d97f092ecd8e6e1ce2c67f0b222ee
Tu Ton <minhtutonthat@gmail.com>
2020-12-22 14:16:03 +0100

    [569693] Filtering add-on impact performances even when it is not enabled on a Capella project
    Bug: 569693
    Change-Id: I091313c9c86b11a01879107f8425377606a960d2
    Signed-off-by: Tu Ton <minhtutonthat@gmail.com>

--- plugins/org.polarsys.capella.filtering.tools/src/org/polarsys/capella/filtering/tools/listeners/FilteringSessionManagerListener.java
+++ plugins/org.polarsys.capella.filtering.tools/src/org/polarsys/capella/filtering/tools/listeners/FilteringSessionManagerListener.java
@@ -12,29 +12,20 @@
  *******************************************************************************/
 package org.polarsys.capella.filtering.tools.listeners;
 
-import org.eclipse.emf.common.notify.Adapter;
-import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.transaction.TransactionalEditingDomain;
 import org.eclipse.sirius.business.api.session.Session;
 import org.eclipse.sirius.business.api.session.SessionListener;
 import org.eclipse.sirius.business.api.session.SessionManagerListener;
-import org.polarsys.capella.common.data.modellingcore.ModelElement;
 import org.polarsys.capella.common.platform.sirius.ted.DataNotifier;
 import org.polarsys.capella.common.platform.sirius.ted.SemanticEditingDomainFactory.SemanticEditingDomain;
-import org.polarsys.capella.filtering.AssociatedFilteringCriterionSet;
-import org.polarsys.capella.filtering.FilteringCriterion;
-import org.polarsys.capella.filtering.FilteringModel;
+import org.polarsys.capella.common.platform.sirius.ted.SemanticEditingDomainFactory.SemanticResourceSet;
+import org.polarsys.capella.filtering.tools.helpers.ViewpointHelper;
+import org.polarsys.kitalpha.ad.services.manager.ViewpointManager;
+import org.polarsys.kitalpha.resourcereuse.model.Resource;
 
 public class FilteringSessionManagerListener extends SessionManagerListener.Stub {
-
-  static Adapter adapter;
-
   public FilteringSessionManagerListener() {
-
-    if (adapter != null) {
-      return;
-    }
-
-    adapter = new FilteringDataListener();
+    // Do nothing
   }
 
   /**
@@ -48,17 +39,43 @@ public class FilteringSessionManagerListener extends SessionManagerListener.Stub
       return;
     }
 
-    DataNotifier dataNotifier = ((SemanticEditingDomain) updated.getTransactionalEditingDomain()).getDataNotifier();
-    switch (notification) {
-    case SessionListener.OPENED:
-      dataNotifier.addAdapter(FilteringCriterion.class, adapter);
-      dataNotifier.addAdapter(FilteringModel.class, adapter);
-      dataNotifier.addAdapter(AssociatedFilteringCriterionSet.class, adapter);
-      dataNotifier.addAdapter(ModelElement.class, adapter);
-      break;
-    case SessionListener.CLOSED:
-      dataNotifier.remove(adapter);
-      break;
+    TransactionalEditingDomain transactionalEditingDomain = updated.getTransactionalEditingDomain();
+    if (transactionalEditingDomain instanceof SemanticEditingDomain) {
+      SemanticEditingDomain semanticED = (SemanticEditingDomain) transactionalEditingDomain;
+      DataNotifier dataNotifier = semanticED.getDataNotifier();
+      SemanticResourceSet rs = semanticED.getResourceSet();
+      switch (notification) {
+      case SessionListener.OPENED:
+        ViewpointManager.getInstance(rs).addListener(new ViewpointManager.Listener2() {
+          @Override
+          public void handleActivation(Resource vp) {
+            FilteringAdapterManager.getInstance().attachAdapter(dataNotifier);
+          }
+
+          @Override
+          public void handleInactivation(Resource vp) {
+            FilteringAdapterManager.getInstance().removeAdapter(dataNotifier);
+          }
+
+          @Override
+          public void handleReferencing(Resource vp) {
+            FilteringAdapterManager.getInstance().attachAdapter(dataNotifier);
+          }
+
+          @Override
+          public void handleUnReferencing(Resource vp) {
+            // Removing adapter will be done with session closed event
+          }
+        });
+        if (ViewpointHelper.isViewpointActive(rs)) {
+          FilteringAdapterManager.getInstance().attachAdapter(dataNotifier);
+        }
+        break;
+      case SessionListener.CLOSED:
+        FilteringAdapterManager.getInstance().removeAdapter(dataNotifier);
+        break;
+      }
     }
   }
+
 }
